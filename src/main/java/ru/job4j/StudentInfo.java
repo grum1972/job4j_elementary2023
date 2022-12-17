5c23b19ddebf7638cff7e83439e889c2bdb3fdb6
Kotov Oleg <grum1972@gmail.com>
2022-12-17 20:27:48 +0300

    4.1. Разбор первой программы. [#159033 #191518]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Oleg Kotov");
+        System.out.println("13/04/1972");
+    }
+}
